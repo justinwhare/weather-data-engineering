10b0f250199f527989860ba1e38385bcbd628461
Justin Whare <justinwhare2@gmail.com>
2025-09-03 18:38:00 -0300

    Initial commit

---
+++ api-request/api_request.py
@@ -0,0 +1,18 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.environ['api_key']
+api_url = f"http://api.weatherstack.com/current?access_key={api_key}&query=Halifax"
+
+def fetch_data():
+    print("Fetching weather data from Weatherstack API...")
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        print("API called successfully.")
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f'An error occurred: {e}')
+        raise
